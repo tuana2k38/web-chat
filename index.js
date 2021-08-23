99acd6d973936ebcbdcb4acb11a5f7602cdcc438
HuyTuan <tuana2k38@gmail.com>
2021-08-23 16:28:39 +0700

    creat app chat

---
+++ index.js
@@ -0,0 +1,22 @@
+const { Socket } = require('dgram')
+const express = require('express')
+const app = express()
+
+const http = require('http')
+const server = http.createServer(app)
+const{
+    Server
+} = require('socket.io')
+
+const io = new Server(server)
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection',(socket)=>{
+    console.log('user connected')
+})
+server.listen(3000,() => {
+    console.log('listen on port 3000')
+})
